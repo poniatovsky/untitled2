7ad6c32713748b851e1b5f4f356c62c23fadc501
poniatovsky <widogie@gmail.com>
2019-11-14 14:34:45 +0100

    hi

--- zajecia4.py
+++ zajecia4.py
@@ -2,3 +2,6 @@ import pandas as pd
 
 df = pd.read_csv(r'C:\Users\tp84481\PycharmProjects\untitled2\venv\Lib\site-packages\numpy\random\tests\data\pcg64-testset-1.csv')
 print(df.seed)
+
+for e in range(10):
+    print(e**2)
